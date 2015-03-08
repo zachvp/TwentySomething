2ffb166fc7827f3d8debcc84389b6a36efc3055a
thatkevinwong <kevinjwo@usc.edu>
2015-03-07 22:11:43 -0800

    Number of changes, check description
     - Added doors that teleport player into other maps.
     - Added main town map
    - We need to specify which entrance each door goes to and save it so
    the player spawns at the right place when they enter a door.
     -

---
+++ UnityProject/Assets/Door.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	public string targetScene; 
+	public GameObject player; 
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("player"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.x == player.transform.position.x && 
+		   gameObject.transform.position.y == player.transform.position.y ){
+			Application.LoadLevel("DialogueTest"); 
+		}
+	}
+}
