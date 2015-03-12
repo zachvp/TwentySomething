893518b15bcf7c42c030ddc5aee14b3811b9504f
Zach Vega-Perkins <vegaperk@usc.edu>
2015-03-11 20:43:26 -0700

    Display choices as list of text options

---
+++ UnityProject/Assets/Scripts/Dialogue/TSDialogueChoiceController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+using Dialogue;
+
+public class TSDialogueChoiceController : MonoBehaviour {
+	//public RectTransform panel;
+	//public Text textChoices;
+
+	private RectTransform panel;
+	private List<Text> _UIChoices;
+
+	private TSDialogueChoiceData _choiceData;
+
+	void Awake() {
+		_UIChoices = new List<Text>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		//Parse("test");
+		//UpdateUI();
+		panel = GameObject.FindGameObjectWithTag("choicePanel").GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Parse (string filename) {
+		_choiceData = TSDialogueParser.Instance.ParseChoices(filename);
+
+		float offset = 16;
+		Vector2 adjustedPosition = panel.position;
+		adjustedPosition.y += offset;
+
+		foreach (string choice in _choiceData.Choices) {
+			//textChoices.text += choice;
+			//textChoices.text += "\n";
+			Text choiceText = Instantiate(Resources.Load("DialogueChoice", typeof(Text))) as Text;
+
+			choiceText.text = choice;
+
+			choiceText.transform.SetParent(panel);
+			choiceText.transform.position = adjustedPosition;
+
+			adjustedPosition.y -= choiceText.rectTransform.rect.height;
+			
+			_UIChoices.Add(choiceText);
+		}
+	}
+
+	private void UpdateUI() {
+		//textChoices.text = "";
+
+	}
+
+	public void ClearUI() {
+		foreach (Text text in _UIChoices) {
+			text.text = "";
+			Destroy(text.gameObject);
+		}
+
+		Resources.UnloadUnusedAssets();
+		_UIChoices.Clear();
+	}
+}
--- UnityProject/Assets/Scripts/Dialogue/TSDialogueController.cs
+++ UnityProject/Assets/Scripts/Dialogue/TSDialogueController.cs
@@ -6,28 +6,31 @@ using Dialogue;
 public class TSDialogueController : MonoBehaviour {
 	public Text textHeader;
 	public Text textBody;
-	public Text textChoices;
+
+	private TSDialogueChoiceController _choiceController;
 
 	private TSDialogueHeaderData _headerData;
 	private TSDialogueBodyData _bodyData;
-	private TSDialogueChoiceData _choiceData;
 
 	private enum State { ENABLED, DISABLED };
 
 	// Use this for initialization
 	void Start () {
+		_choiceController = gameObject.AddComponent<TSDialogueChoiceController>();
+
 		ClearUI();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 	}
 
 	// public methods
 	public void Parse(string filename) {
 		_headerData = TSDialogueParser.Instance.ParseHeader(filename);
 		_bodyData   = TSDialogueParser.Instance.ParseBody(filename);
-		_choiceData = TSDialogueParser.Instance.ParseChoices(filename);
+		_choiceController.Parse(filename);
 
 		UpdateUI();
 	}
@@ -39,17 +42,12 @@ public class TSDialogueController : MonoBehaviour {
 	private void UpdateUI() {
 		textHeader.text = _headerData.Header;
 		textBody.text = _bodyData.Body;
-		
-		textChoices.text = "";
-		foreach (string choice in _choiceData.Choices) {
-			textChoices.text += choice;
-			textChoices.text += "\n";
-		}
 	}
 
 	private void ClearUI() {
 		textHeader.text = "";
 		textBody.text = "";
-		textChoices.text = "";
+		_choiceController.ClearUI();
+		//textChoices.text = "";
 	}
 }
